8e6272f8b13fbd89110846a00ecf16c5b005837d
Justin Herndon <jherndon111@gmail.com>
2025-02-15 15:52:38 -0600

    removing home-manager

--- flake.nix
+++ flake.nix
@@ -3,10 +3,6 @@
   # Rebuild using nixos-rebuild switch --flake '/path/to/flake/directory#hostname` --impure
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
   outputs = { nixpkgs, home-manager, ... }: {
     nixosConfigurations.home-server = nixpkgs.lib.nixosSystem {
@@ -14,11 +10,6 @@
       modules = [ 
           ./home-server 
      
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-          }
      ];
     };
   };
