1c7a3f6673ccd0bb642b2aca2372ff94b2cd216e
Justin Herndon <jherndon111@gmail.com>
2025-05-28 16:56:59 -0500

    trying out one service in caddy

--- home-server/default.nix
+++ home-server/default.nix
@@ -61,14 +61,13 @@ in
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
       hash = "sha256-Gsuo+ripJSgKSYOM9/yl6Kt/6BFCA6BuTDvPdteinAI=";
     };
-    config =
-    ''
-      jusanhomelab.com {
-        tls {
-          dns cloudflare ${CLOUDFLARE_API_TOKEN}
-        }
-      }
+    globalConfig = ''
+    acme_dns cloudflare ${CLOUDFLARE_API_TOKEN}
     '';
+    
+    virtualHosts."budget.jusanhomelab.com" = {
+      extraConfig = " reverse_proxy localhost:3000"
+    };
   };
 
   networking.firewall.allowedTCPPorts = [ 80 443 3000 8000 8096 ];
