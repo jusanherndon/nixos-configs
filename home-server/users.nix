7ed7c9815417894e025d20429c5d96c4b84190f4
Justin Herndon <jherndon111@gmail.com>
2026-01-03 10:37:12 -0600

    forgot to remove jellyfin user from users files

--- home-server/users.nix
+++ home-server/users.nix
@@ -3,27 +3,10 @@
       justin = {
         isNormalUser = true;
         description = "Justin";
-        extraGroups = [ "networkmanager" "wheel" "deluge" "jellyfin" ];
+        extraGroups = [ "networkmanager" "wheel" "deluge" ];
       };
-
       deluge = {
         extraGroups = [ "deluge" ];
       };
-
-      jellyfin = {
-        description = "Jellyfin daemon user";
-        extraGroups = [ "jellyfin" ];
-      };
   };
- # home-manager.users.justin = {
- # programs.home-manager.enable = true;
-  
- # programs.git = {
- #   enable = true;
- #   userName = "Justin Herndon";
- #   userEmail = "jherndon111@gmail.com";
- # };
-  
- # home.stateVersion = "25.05";
-#};
 }
