1f611476e0592ae4c533e9a0a1dfb09c89d5d3d6
Justin Herndon <jherndon111@gmail.com>
2026-01-12 15:55:41 -0600

    cleaning up the configs with new style

--- home-desktop/default.nix
+++ home-desktop/default.nix
@@ -7,16 +7,22 @@
       ./services.nix
     ];
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.supportedFilesystems = [ "nfs" ];
-  boot.loader.grub.useOSProber = true;
-  networking.networkmanager.enable = true;
-  networking.hostName = "nixos-laptop"; # Define your hostname.
+  boot = {
+    loader = { 
+      grub = { 
+        enable = true;
+        device = "/dev/sda";
+        useOSProber = true;
+      };
+    };
+    supportedFilesystems = [ "nfs" ];
+  };
+  networking = {
+    networkmanager.enable = true;
+    hostName = "nixos-laptop";
+  };
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  services.rpcbind.enable = true; # needed for NFS
 
   time.timeZone = "America/Chicago";
 
@@ -45,13 +51,14 @@
     fzf
     mtr
     tailscale
-    kdePackages.kate
   ];
-
 
   users.users.justin = {
     isNormalUser= true;
     extraGroups = [ "networkManager" "wheel" "deluge" "jellyfish" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    ];
   };
 
   system.stateVersion = "24.11";
--- home-desktop/services.nix
+++ home-desktop/services.nix
@@ -1,45 +1,43 @@
 {
-  services.openssh.enable = true;
-  services.printing.enable = true;
-  services.pulseaudio.enable = false;
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
+  services = {
+    openssh.enable = true;
+    rpcbind.enable = true; # needed for NFS
+    #printing.enable = true;
+    pulseaudio.enable = false;
+    displayManager.sddm.enable = true;
+    desktopManager.plasma6.enable = true;
+    rtkit.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+    xserver.xkb = {
+      layout = "us";
+      variant = "";
+    };
+    tailscale = {
+      enable = true;
+      useRoutingFeatures = "both";
+    };
   };
 
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  systemd.mounts = [{
+  systemd = {
+    mounts = [{
       type = "nfs";
         mountConfig = {
           Options = "noatime";
       };
     what = "openmediavault.lan:/nas";
     where = "/mnt/nas";
-  }];
-
-  systemd.automounts = [{
-    wantedBy = [ "multi-user.target" ];
-    automountConfig = {
-      TimeoutIdleSec = "0";
-    };
-    where = "/mnt/nas";
-  }];
-
-  services.tailscale = {
-    enable = true;
-    useRoutingFeatures = "both";
+    }];
+    automounts = [{
+      wantedBy = [ "multi-user.target" ];
+      automountConfig = {
+        TimeoutIdleSec = "0";
+      };
+      where = "/mnt/nas";
+    }];
   };
-
 }
